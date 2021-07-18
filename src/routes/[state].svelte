3c2c64a191573c9bca062e1eed7704696685f4b2
Yusron Hanan <yusronzain@gmail.com>
2021-07-18 16:52:49 +0700

    make pages and components files

---
+++ src/routes/[state].svelte
@@ -0,0 +1,20 @@
+<!-- <script context="module"> ==> the call is performed before creating the component. -->
+<script context="module">
+  export function preload(page) {
+    console.log(page);
+    return { state: page.params["state"] };
+  }
+</script>
+
+<script>
+  import CovidStat from "../components/CovidStat.svelte";
+  import CovidChart from "../components/CovidChart.svelte";
+  export let state;
+</script>
+
+<h1>
+  {state}
+</h1>
+
+<CovidStat />
+<CovidChart />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,16 @@
 <script>
+  import TableContainer from "../components/TableContainer.svelte";
+  import CovidStat from "../components/CovidStat.svelte";
+  import CovidChart from "../components/CovidChart.svelte";
 </script>
 
 <svelte:head>
   <title>Sapper project template</title>
 </svelte:head>
+
+<TableContainer />
+<CovidStat />
+<CovidChart />
 
 <style>
 </style>
